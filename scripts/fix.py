254627e0412a9cc459baa0860ee2b0df2dd56f90
molarmanful <benjinpang@gmail.com>
2024-04-03 13:47:38 -0400

    npm

--- scripts/fix.py
+++ scripts/fix.py
@@ -1,9 +1,11 @@
+f.os2_version = 4
+f.os2_vendor = "BenP"
 f.os2_panose = (2, 0, 6, 9, 0, 0, 0, 0, 0, 0)
 f.os2_weight_width_slope_only = True
 f.selection.all()
+f.em = 2048
 f.correctDirection()
 f.removeOverlap()
-f.em = 2048
 for g in f:
     if f[g].unicode != -1:
         f[g].glyphname = nameFromUnicode(f[g].unicode, "AGL with PUA")
