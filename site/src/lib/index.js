b6f57d556490511828650bb6cb1e2134560f75ba
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-09-19 22:29:41 -0400

    ouch

--- site/src/lib/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './Header.svelte'
-
-export const randitem = xs => xs[0 | (Math.random() * xs.length)]
--- site/svelte.config.js
+++
@@ -1,7 +0,0 @@
-import adapter from '@sveltejs/adapter-static';
-
-export default {
-	kit: {
-		adapter: adapter()
-	}
-};
