33740a7e70508a9e445425b18a69baa21810aa6f
molarmanful <ben@benpa.ng>
2025-02-22 19:51:04 -0500

    update builder

--- flake.nix
+++ flake.nix
@@ -3,85 +3,71 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    bited-utils = {
-      url = "github:molarmanful/bited-utils";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
+    systems.url = "systems";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+    bited-utils.url = "github:molarmanful/bited-utils";
   };
 
   outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      bited-utils,
-      ...
-    }:
+    inputs@{ systems, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.bited-utils.flakeModule
+      ];
+      systems = import systems;
+      perSystem =
+        { config, pkgs, ... }:
+        {
 
-    let
-      name = "kirsch";
-      version = builtins.readFile ./VERSION;
-    in
-
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        bupkgs = bited-utils.packages.${system};
-      in
-      rec {
-
-        packages =
-          let
-            build =
-              o:
-              pkgs.callPackage ./. (
-                {
-                  inherit version;
-                  inherit (bupkgs) bited-build;
-                }
-                // o
-              );
-          in
-          {
-            ${name} = build { pname = name; };
-            "${name}-nerd" = build {
-              pname = "${name}-nerd";
-              nerd = true;
-            };
-            "${name}-release" = build {
-              pname = "${name}-release";
-              nerd = true;
-              release = true;
-            };
-            "${name}-img" = pkgs.callPackage ./img.nix {
-              inherit (bupkgs) bited-img;
-              name = "${name}-img";
-            };
-            default = packages.${name};
+          bited-utils = {
+            name = "kirsch";
+            version = builtins.readFile ./VERSION;
+            src = ./.;
+            nerd = true;
           };
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            nil
-            nixd
-            nixfmt-rfc-style
-            statix
-            deadnix
-            marksman
-            markdownlint-cli
-            actionlint
-            taplo
-            mdformat
-            python313Packages.mdformat-gfm
-            python313Packages.mdformat-gfm-alerts
-            bupkgs.bited-clr
-          ];
-        };
+          devshells.default = {
 
-      }
-    );
+            commands = with pkgs; [
+              {
+                package = nil;
+                category = "lsp";
+              }
+              {
+                package = nixd;
+                category = "lsp";
+              }
+              {
+                package = nixfmt-rfc-style;
+                category = "formatter";
+              }
+              {
+                package = statix;
+                category = "linter";
+              }
+              {
+                package = deadnix;
+                category = "linter";
+              }
+              { package = taplo; }
+              {
+                package = marksman;
+                category = "lsp";
+              }
+              {
+                package = mdformat;
+                category = "formatter";
+              }
+              { package = config.bited-utils.bited-clr; }
+            ];
+
+            packages = with pkgs; [
+              python313Packages.mdformat-gfm
+              python313Packages.mdformat-gfm-alerts
+            ];
+          };
+        };
+    };
 }
